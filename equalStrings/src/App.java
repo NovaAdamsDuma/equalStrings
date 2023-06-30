beb8f6825e8b4d729669bb64b7a1d99efa816f46
NovaAdamsDuma <137451209+NovaAdamsDuma@users.noreply.github.com>
2023-06-30 18:11:51 +0200

    Update
    True being returned, and false being returned, however:
    - s not being transformed into taget

--- equalStrings/src/App.java
+++ equalStrings/src/App.java
@@ -16,20 +16,44 @@ public class App {
                 tZero++;
             }
         }
+        //If there is an unequal amount of 1s and 0s, it is impossible to equate s to target
+        if(sZero != tZero){
+            return false;
+        }
+        // iterate over and check if each char is equal
+        else{
+            for (int x = 0; x <= n-1; x++) {
+                char i = s.charAt(x);
+                // if not, choose i and j in s to make equal to target --> next iteration
+                //****************** */
+                if(s.charAt(x)!=target.charAt(x)){
+                    for (int y=x+1; y<n-1; y++){
+                        char j = s.charAt(y);
+                        if(s.charAt(y)!= s.charAt(x)){
+                            s = s.replace(s.charAt(x),j);
+                            s = s.replace(s.charAt(y),i);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(s);
+        return true;
         //System.out.println("s zeros: " + sZero + "\n target zeros: " + tZero);
 
-        return false;
+        
     }
 
     public static void main(String[] args) throws Exception {
-        String s = "1010";
-        String target = "0110";
+        String s = "0110";
+        String target = "0101";
 
-        setEqual(s, target);
+        System.out.println(setEqual(s, target));
 
         // 1st condition: check if equal 1's and 0's
         // iterate over and check if each char is equal
-        // if not, choose i and j in s to make equal to target --> next iteration
+        
         // iterate over remaining characters and check if each char is equal
         // if not, choose i and j in s to make equal to target --> next iteration......
         // return true or false
